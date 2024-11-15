9e0587946b474f7b108a9ba76a6f02dc36f34af1
Liz (Judd) Byrne <liz.byrne@guild.com>
2024-11-15 14:47:17 -0500

    feat: update semantic-release version and streamline dependencies

--- utilities/release/src/base.config.js
+++ utilities/release/src/base.config.js
@@ -3,13 +3,8 @@ module.exports.baseConfig = packageName => {
   return {
     extends: 'semantic-release-monorepo',
     branches: [
-      { name: 'PLAT-133-composite-actions-monorepo', prerelease: 'beta' },
       { name: 'main' },
-      { name: 'beta', channel: 'next', prerelease: true },
-
-      // Any branch starting with `alpha-` will be auto-released
-      // as a pre-release (e.g. `1.3.0-alpha-add-cool-new-feature.1`)
-      { name: 'alpha-*', prerelease: true },
+      { name: 'PLAT-133-composite-actions-monorepo', prerelease: 'beta' }
     ],
     ci: false,
     debug: true,
@@ -22,10 +17,7 @@ module.exports.baseConfig = packageName => {
             { breaking: true, release: 'major' },
             { revert: true, release: 'patch' },
             { type: 'breaking', release: 'major' },
-            { type: 'major', release: 'major' },
-            { type: 'release', release: 'major' },
             { type: 'feat', release: 'minor' },
-            { type: 'minor', release: 'minor' },
             { type: 'fix', release: 'patch' },
             { type: 'patch', release: 'patch' },
             { type: 'perf', release: 'patch' },
@@ -37,12 +29,11 @@ module.exports.baseConfig = packageName => {
       // It uses release-notes-generator under the hood, but also adds a link to the Jira ticket
       // found in the pull request description.
       '@semantic-release/release-notes-generator',
-      '@semantic-release/changelog',
       [
         '@semantic-release/github',
         {
           // long-term we might want the GH URL to be more dynamic for other projects
-          successComment: `:tada: This \${issue.pull_request ? 'pull request' : 'issue'} is included in version \${nextRelease.version} of ${packageName} :tada:\n\nThe release is available on:\n- [npm package (\${nextRelease.channel || '@latest'} dist-tag)](https://www.npmjs.com/package/${packageName}/v/\${nextRelease.version})\n- [GitHub release](https://github.com/GuildEducationInc/recess-design-system/releases/tag/${packageName.replace(
+          successComment: `:tada: This \${issue.pull_request ? 'pull request' : 'issue'} is included in version \${nextRelease.version} of ${packageName} :tada:\n\nThe release is available in [GitHub releases](https://github.com/GuildEducationInc/github-actions/releases/tag/${packageName.replace(
             '@',
             '%40',
           )}-v\${nextRelease.version})\n\nYour **[semantic-release](https://github.com/semantic-release/semantic-release)** bot :package::rocket:`,
